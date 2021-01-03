63729cfe964b78352edf39ffe6cbea1962817612
Markdarkness <mv10marcosmv@gmail.com>
2021-01-03 17:27:06 -0300

    wayne: use qualcomm Assertive Display Calibration

--- device_prop.mk
+++ device_prop.mk
@@ -81,6 +81,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hist.brightness.threshold=5 \
     vidc.enc.dcvs.extra-buff-count=2 \
 
+# System property for cabl
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.cabl=0 \
+    ro.vendor.display.cabl=0 \
+    ro.vendor.display.ad=1 \
+    ro.vendor.display.ad.sdr_calib_data=/data/vendor/display/ad_calib.cfg \
+    ro.vendor.display.sensortype=2
+
 # Dual SIM
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds
